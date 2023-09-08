8236e22f69a97b6352f51c9e4a238290b5e93550
Parth Makadiya <66172036+makadiyaparth@users.noreply.github.com>
2023-09-08 19:58:48 +0530

    Add value comparator

---
+++ pages/value-comparator/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    const compareBtn = $("#compare-btn");
+    const addPairBtn = $("#add-pair-btn");
+    const resetBtn = $("#reset-btn");
+    const resultElement = $("#result");
+    const inputSection = $(".input-section");
+
+    const pair = `
+        <div class="pair">
+            <input type="text" inputmode="numeric" class="measurement" placeholder="Measurement">
+            <input type="text" inputmode="numeric" class="price" placeholder="Price">
+        </div>
+    `;
+
+    reset();
+
+    compareBtn.click(function() {
+        const pairs = $(".pair");
+        let bestDeal = null;
+
+        pairs.each(function() {
+            const measurement = $(this).find(".measurement").val();
+            const price = parseFloat($(this).find(".price").val());
+
+            const value = price / measurement;
+            if (!isNaN(value)) {
+                if (!bestDeal || value < bestDeal.value) {
+                    bestDeal = { measurement, price, value };
+                }
+            }
+        });
+
+        if (bestDeal) {
+            resultElement.text(`Best deal is ${bestDeal.measurement} for $${bestDeal.price}`);
+        } else {
+            resultElement.text("No valid deals found.");
+        }
+    });
+
+    addPairBtn.click(function() {
+        $(".input-section").append(pair);
+    });
+
+    resetBtn.click(reset);
+    
+    function reset() {
+        inputSection.empty();
+        inputSection.append(pair);
+        inputSection.append(pair);
+        resultElement.empty();
+    }
+});
